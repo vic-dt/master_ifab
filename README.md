4b0527884b61128efa1ff7faba35f32edb183242
vic-dt <victoriavr1323@gmail.com>
2025-05-06 17:25:35 +0200

    Proyecto inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# master_ifab
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/master_ifab/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.master_ifab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
